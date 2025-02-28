1e27239f7f298a0de95b8fbb8231cfd6ffd60c41
cleot <cleot@users.noreply.github.com>
2025-02-28 21:48:02 +0000

    Update RPC servers

--- alfajores/rpc-servers.js
+++ alfajores/rpc-servers.js
@@ -1,7 +1,7 @@
 // Alfajores testnet RPC server list
-// This file can be updated by "celcli call" to fetch registered RPC servers and check their health
+// This file can be updated by "celocli network:rpc-urls" to fetch registered RPC servers and check their health
+// Last updated: 2025-02-28T21:48:02.386Z
 
 export const backendList = [
-  'https://alfajores-forno.celo-testnet.org',
-  // Add additional alfajores testnet RPC endpoints here
+  'https://alfajores-forno.celo-testnet.org'
 ];
