8195bf8ca3960098c5150e0fcc90714dca745741
cleot <cleot@users.noreply.github.com>
2025-03-03 01:10:02 +0000

    Update RPC servers

--- baklava/rpc-servers.js
+++ baklava/rpc-servers.js
@@ -3,5 +3,6 @@
 
 export const backendList = [
   'https://celo-baklava-dev.atweb3.dev',
-  'https://baklava-forno.celo-testnet.org'
+  'https://baklava-forno.celo-testnet.org',
+  'https://baklava-rpc.happycelo.com             0xB7349B64D244563675643c0D30292b5CAC5dD772'
 ];
