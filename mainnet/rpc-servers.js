a20966e403025d27aa6848061d353f1438b04409
cleot <cleot@users.noreply.github.com>
2025-03-25 01:11:44 +0000

    Update RPC servers

--- mainnet/rpc-servers.js
+++ mainnet/rpc-servers.js
@@ -2,5 +2,7 @@
 // This file can be updated by "celocli network:rpc-urls" to fetch registered RPC servers and check their health
 
 export const backendList = [
-  'https://forno.celo.org'
+  'https://forno.celo.org',
+  'https://celo.newroad.network',
+  'https://celo.chainvibes.nl'
 ];
