070be9f9d93e7fe66409958995751c28330f94fb
cleot <7118445+cleot@users.noreply.github.com>
2025-03-25 10:38:05 +0000

    Deploying to gh-pages from @ atweb3-io/celo-community-rpc@47738855432f2e1271afe8ad36114e23a2b6644c 🚀

--- network/mainnet/rpc-servers.js
+++ network/mainnet/rpc-servers.js
@@ -2,5 +2,7 @@
 // This file is automatically updated by the RPC server update workflow
 
 export const servers = [
-  'https://forno.celo.org'
+  'https://forno.celo.org',
+  'https://celo.newroad.network',
+  'https://celo.chainvibes.nl'
 ];
