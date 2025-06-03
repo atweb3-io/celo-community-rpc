c40145ee5096920e1eb1d5ceeee42432188c45bc
cleot <cleot@users.noreply.github.com>
2025-06-03 17:03:19 +0000

    Update RPC servers

--- public/network/baklava/rpc-servers.js
+++ public/network/baklava/rpc-servers.js
@@ -7,12 +7,9 @@ export const servers = [
   'https://celo-rpc-baklava.swiftstaking.com',
   'https://baklava.moonli.me',
   'https://baklava-rpc.happycelo.com',
-  'https://baklava.chainvibes.nl',
   'https://baklava.staking.fund',
   'https://celo-rpc-01.baklava.dsrvlabs.dev',
   'http://rpcb.usopp.club/',
-  'https://baklava-rpc.validator.capital',
   'https://celo-testnet-rpc-01.stakely.io/',
-  'https://validator-0.baklava.celo-testnet.org',
   'https://celo-baklava-rpc--anchorage-1.staking.production.figment.io'
 ];
